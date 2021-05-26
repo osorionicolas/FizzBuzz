310ddd0ce2ae7f3a10c3c8c8a23d9131c858f70d
osorionicolas <osorionicolas95@gmail.com>
2021-05-26 17:02:56 -0300

    Remove unnecessary code

--- src/main/java/com/nosorio/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/nosorio/fizzbuzz/FizzBuzz.java
@@ -1,41 +1,29 @@
 package com.nosorio.fizzbuzz;
 
-import java.util.List;
-import java.util.stream.Collectors;
+
 import java.util.stream.IntStream;
 
 public class FizzBuzz {
 	
 	private static String FIZZ = "Fizz";
 	private static String BUZZ = "Buzz";
-	private static String FIZZBUZZ = "FizzBuzz";
 	
-    private static List<Integer> arrayInteger = IntStream.rangeClosed(1, 100).boxed().collect(Collectors.toList());
-
 	public static void main(String[] args) {
-        arrayInteger.stream().forEach(number -> System.out.println(verifyNumber(number)));	
+		IntStream.rangeClosed(1, 100)
+				 .boxed()
+				 .forEach(number -> System.out.println(verifyNumber(number)));	
 	}
 	
 	public static String verifyNumber(Integer number) {
-		if(fizz(number) && buzz(number)) {
-			return FIZZBUZZ;
-		}
-		else if(fizz(number)) {
-			return FIZZ;
-		}
-		else if(buzz(number)) {
-			return BUZZ;
-		}
-		else {
-			return number.toString();
-		}
+		String response = fizz(number) + buzz(number);
+		return response.isEmpty() ? number.toString() : response;
 	}
 	
-	private static Boolean fizz(Integer number) {
-		return number % 3 == 0;
+	private static String fizz(Integer number) {
+		return number % 3 == 0 ? FIZZ : "";
 	}
 	
-	private static Boolean buzz(Integer number) {
-		return number % 5 == 0;
+	private static String buzz(Integer number) {
+		return number % 5 == 0 ? BUZZ : "";
 	}
 }
